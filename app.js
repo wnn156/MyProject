82412424bb11d97e5bcff008a984ff553b1c4787
jun <wnn156@naver.com>
2019-08-30 22:00:50 +0900

    mongodb complete

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var stylus = require('stylus');
+// [LOAD PACKAGES]
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+var Book = require('./models/book');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// [CONFIGURE APP TO USE bodyParser]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-var app = express();
+// [CONFIGURE SERVER PORT]
+var port = process.env.PORT || 8080;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// [CONFIGURE ROUTER]
+var router = require('./routes')(app, Book)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// [RUN SERVER]
+var server = app.listen(port, function(){
+  console.log("Express server has started on port " + port)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+var mongoose = require('mongoose');
+
+// ......
+
+// [ CONFIGURE mongoose ]
+
+// CONNECT TO MONGODB SERVER
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
 });
+
+mongoose.connect('mongodb://localhost:27017/test');
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,40 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function (app, Book) {
+  // GET ALL BOOKS
+  app.get('/test/books', function (req, res) {
+    Book.find(function (err, books) {
+      if (err) return res.status(500).send({ error: 'database failure' });
+      res.json(books);
+    })
+  });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Appengine is updated' });
-  console.log('Hello Appengine is updated');
-});
 
-module.exports = router;
+  // CREATE BOOK
+  app.post('/test/books', function (req, res) {
+    var book = new Book();
+    book.title = req.body.name;
+    book.author = req.body.author;
+    book.published_date = new Date(req.body.published_date);
+
+    book.save(function (err) {
+      if (err) {
+        console.error(err);
+        res.json({ result: 0 });
+        return;
+      }
+
+      res.json({ result: 1 });
+
+    });
+  });
+
+  // UPDATE THE BOOK
+  app.put('/api/books/:book_id', function (req, res) {
+    res.end();
+  });
+
+  // DELETE BOOK
+  app.delete('/api/books/:book_id', function (req, res) {
+    res.end();
+  });
+
+}
